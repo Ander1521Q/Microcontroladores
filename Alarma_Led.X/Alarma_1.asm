ea9082c6c6cbc64bc5d0ebf583fb1b881762e2fa
Ander1521Q <andersonmurbano@unicauca.edu.co>
2025-08-27 12:36:06 -0500

    Avance codigo Alarma_Led para pruebas

--- Alarma_Led.X/Alarma_1.asm
+++ Alarma_Led.X/Alarma_1.asm
@@ -10,48 +10,68 @@
   CONFIG  PBADEN = OFF          ;Configura los PORTB como digitales
   CONFIG  LVP = OFF             ; Desactiva la programacion a bajo voltage
 
-#include <xc.inc> ;Incluir definiciones para el PIC18F4550
+  
+;Incluir definiciones para el PIC18F4550
+#include <xc.inc> 
   
   PSECT resetVec, class=CODE, reloc=2 ;Vector de reinicio
   
   ORG 0x00
-  GOTO Inicio
+  GOTO Inicio ;Va a Inicio
   
-  PSECT main_code, clss=CODE, reloc=2 ;Codigo principal
+  PSECT main_code, class=CODE, reloc=2 ;Codigo principal
   
-  Inicio
-    CLRF TRISB
-    CLRF LATB
+  Inicio:
     
-  Loop
-    BSF LATB, 0
-    CALL Retardo_5s
+    CLRF TRISB ;Configura los PORTB como salida
+    CLRF LATB ;Configura PORTB en 0
+    GOTO Encendido ;Va a Encendido
     
-    BCF LATB, 0
-    CALL Retardo_2s
+  Encendido:
+    BSF LATB, 0 ;Coloca 5v en RB0
+    MOVLW 5 ;Carga 5 en W
+    MOVWF ContadorSegs ;Guarda 5 en la variable ContadorSegs
     
-    GOTO Loop
+ EncendidoLoop:
+    CALL Retardo_1s ;Llama a la subrutina retardo_1s
+    DECFSZ ContadorSegs, F ;Decrementa ContadorSegs, si llega a 0 salta a la siguiente
+    GOTO EncendidoLoop ;Si no llega a 0, repite
+    GOTO Apagado ;Va a la rutina apagado, cuando llega a 0
     
-;=====================================
-;Subrutinas de retardos
-;=====================================
+ Apagado:
+    BCF LATB, 0 ;Apaga el Led, RB0 en 0v
+    MOVLW 2 ;Carga 2 en W
+    MOVWF ContadorSegs ;Guarda el 2 en la variable ContadorSegs
+    
+ ApagadoLoop:
+    CALL Retardo_1s ;llama al retardo_1s
+    DECFSZ ContadorSegs, F ;Decrementa ContadorSegs, si llega a 0 salta
+    GOTO ApagadoLoop ;Si no es 0, repite el bucle
+    GOTO Encendido ;Cuando termina vuelve a encender el Led
 ; === Retardo 1s ====
   Retardo_1s:
-    MOVLW 25
-    MOVWF ContadorExterno
+    MOVLW 25 ;Carga 25 en w (contador externo)
+    MOVWF ContadorExterno ;Guardaen la variable ContadorExterno
     
   LoopExterno:
-    MOVLW 250
-    MOVWF ContadorInterno
+    MOVLW 250 ;Carga 25 en W (contador interno)
+    MOVWF ContadorInterno ; Guarda en la variable ContadorExterno 
     
   LoopInterno:
-    NOP
-    NOP
-    NOP
-    DECFSZ ContadorInterno
-    GOTO LoopInterno
+    NOP ;Consume un ciclo
+    NOP ;Consume un ciclo
+    NOP ;Consume un ciclo
+    DECFSZ ContadorInterno, F ;Decrementa el contador interno, si no es 0 repite
+    GOTO LoopInterno ;Repite hasta llegar a 0
+    DECFSZ ContadorExterno, F ;Cuando Interno llega a 0, decrementa el externo
+    GOTO LoopExterno ;Si no es 0, repite el externo
     
-    DECFSZ ContadorExterrno
-    GOTO LoopExterno
+    RETURN ;Cuando ambos llegan a 0 se regresa
     
-    RETURN
+    PSECT udata
+    ContadorExterno: DS 1 ;Reserva 1 byte para el contador externo
+    ContadorInterno: DS 1 ;Reserva 1 byte para el contador interno
+    ContadorSegs: DS 1 ;Reserva 1 byte para los segundos
+ 
+  END ;Fin del codigo
+   
